70b07d3013becc544aef70566ef85a6145477d3f
Michael Rosenberger <michael.rosenberger@aeb.com>
2025-08-08 14:59:15 +0200

    Adds idps

---
+++ src/Resource/IdentityProviders.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Fschmtt\Keycloak\Resource;
+
+use Fschmtt\Keycloak\Collection\IdentityProviderCollection;
+use Fschmtt\Keycloak\Collection\UserCollection;
+use Fschmtt\Keycloak\Http\Criteria;
+use Fschmtt\Keycloak\Http\Query;
+use Fschmtt\Keycloak\Representation\IdentityProvider;
+use Fschmtt\Keycloak\Representation\User as UserRepresentation;
+
+class IdentityProviders extends Resource
+{
+    public function all(?Criteria $criteria = null, ?string $realm = null): IdentityProviderCollection
+    {
+        $realm = $this->getRealm($realm);
+        return $this->queryExecutor->executeQuery(
+            new Query(
+                ' /admin/realms/{realm}/identity-provider/instances',
+                UserCollection::class,
+                [
+                    'realm' => $realm,
+                ],
+                $criteria,
+            ),
+        );
+    }
+
+    public function get(string $alias, ?string $realm = null): IdentityProvider
+    {
+        $realm = $this->getRealm($realm);
+        return $this->queryExecutor->executeQuery(
+            new Query(
+                '/admin/realms/{realm}identity-provider/instances/{alias}',
+                UserRepresentation::class,
+                [
+                    'realm' => $realm,
+                    'alias' => $alias,
+                ],
+            ),
+        );
+    }
+}
