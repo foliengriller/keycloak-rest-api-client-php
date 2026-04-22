e492f6a719b91cb1af247af085ef69cb556f1e8f
Michael Rosenberger <michael.rosenberger@aeb.com>
2026-04-22 13:13:12 +0200

    feat: adds more idp functions

--- src/Resource/IdentityProviders.php
+++ src/Resource/IdentityProviders.php
@@ -10,6 +10,7 @@ use Fschmtt\Keycloak\Http\Command;
 use Fschmtt\Keycloak\Http\Criteria;
 use Fschmtt\Keycloak\Http\Method;
 use Fschmtt\Keycloak\Http\Query;
+use Fschmtt\Keycloak\Json\JsonDecoder;
 use Fschmtt\Keycloak\Representation\IdentityProvider;
 use Fschmtt\Keycloak\Representation\IdentityProviderMapper;
 use Psr\Http\Message\ResponseInterface;
@@ -36,8 +37,8 @@ class IdentityProviders extends Resource
         $realm = $this->getRealm($realm);
         return $this->queryExecutor->executeQuery(
             new Query(
-                '/admin/realms/{realm}identity-provider/instances/{alias}',
-                IdentityProviderCollection::class,
+                '/admin/realms/{realm}/identity-provider/instances/{alias}',
+                IdentityProvider::class,
                 [
                     'realm' => $realm,
                     'alias' => $alias,
@@ -46,12 +47,12 @@ class IdentityProviders extends Resource
         );
     }
 
-    public function create(IdentityProvider $identityProvider, ?string $realm = null): void
+    public function create(IdentityProvider $identityProvider, ?string $realm = null): ResponseInterface
     {
         $realm = $this->getRealm($realm);
-        $this->commandExecutor->executeCommand(
+        return $this->commandExecutor->executeCommand(
             new Command(
-                '/admin/realms/{realm}/identity-provider/instances/{alias}',
+                '/admin/realms/{realm}/identity-provider/instances',
                 Method::POST,
                 [
                     'realm' => $realm,
@@ -59,6 +60,29 @@ class IdentityProviders extends Resource
                 $identityProvider,
             ),
         );
+    }
+
+    /**
+     * @return array<mixed>
+     */
+    public function importConfig(string $providerId, string $fromUrl, ?string $realm = null): array
+    {
+        $realm = $this->getRealm($realm);
+        $response = $this->commandExecutor->executeCommand(
+            new Command(
+                '/admin/realms/{realm}/identity-provider/import-config',
+                Method::POST,
+                [
+                    'realm' => $realm,
+                ],
+                [
+                    'providerId' => $providerId,
+                    'fromUrl' => $fromUrl,
+                ],
+            ),
+        );
+
+        return (new JsonDecoder())->decode($response->getBody()->getContents());
     }
 
     public function getMappers(string $alias, ?string $realm = null): IdentityProviderMapperCollection
@@ -88,6 +112,55 @@ class IdentityProviders extends Resource
                     'alias' => $alias,
                 ],
                 $identityProviderMapper,
+            ),
+        );
+    }
+
+    public function update(string $alias, IdentityProvider $identityProvider, ?string $realm = null): ResponseInterface
+    {
+        $realm = $this->getRealm($realm);
+        return $this->commandExecutor->executeCommand(
+            new Command(
+                '/admin/realms/{realm}/identity-provider/instances/{alias}',
+                Method::PUT,
+                [
+                    'realm' => $realm,
+                    'alias' => $alias,
+                ],
+                $identityProvider,
+            ),
+        );
+    }
+
+    public function updateMapper(IdentityProviderMapper $mapper, string $alias, ?string $realm = null): ResponseInterface
+    {
+        $realm = $this->getRealm($realm);
+        return $this->commandExecutor->executeCommand(
+            new Command(
+                '/admin/realms/{realm}/identity-provider/instances/{alias}/mappers/{id}',
+                Method::PUT,
+                [
+                    'realm' => $realm,
+                    'alias' => $alias,
+                    'id' => $mapper->getId(),
+                ],
+                $mapper,
+            ),
+        );
+    }
+
+    public function deleteMapper(string $mapperId, string $alias, ?string $realm = null): ResponseInterface
+    {
+        $realm = $this->getRealm($realm);
+        return $this->commandExecutor->executeCommand(
+            new Command(
+                '/admin/realms/{realm}/identity-provider/instances/{alias}/mappers/{id}',
+                Method::DELETE,
+                [
+                    'realm' => $realm,
+                    'alias' => $alias,
+                    'id' => $mapperId,
+                ],
             ),
         );
     }
